996c1a019ac05a1401b7943ecefa498b17f022ea
Sa-So <60461699+Sa-So@users.noreply.github.com>
2020-10-22 00:27:01 +0530

    Create fun&dic.py

---
+++ Week 5/fun&dic.py
@@ -0,0 +1,12 @@
+def printdic():
+  n=int(input())
+  print('{',end='')
+  for i in range (1,n+1):
+    s=i*i
+    if i<n:
+  	  print(i,': ',s,",",sep='',end=' ')
+    else:
+  	  print(i,': ',s,sep='',end='')
+  print('}',end='')
+  
+ printdic()
